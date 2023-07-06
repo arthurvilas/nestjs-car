1cfa559f6622c20201a0e7f9aab0c558521cf60c
Arthur Santos <arthurvilas5@gmail.com>
2023-07-05 21:34:19 -0300

    Finish User  endpoints

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Repository } from 'typeorm';
 import { User } from './user.entity';
 import { InjectRepository } from '@nestjs/typeorm';
@@ -25,7 +25,7 @@ export class UsersService {
   async update(id: number, attributes: Partial<User>) {
     const user = await this.findOne(id);
     if (!user) {
-      throw new Error('User not found');
+      throw new NotFoundException('User not found');
     }
     Object.assign(user, attributes);
     return this.repo.save(user);
@@ -34,7 +34,7 @@ export class UsersService {
   async remove(id) {
     const user = await this.findOne(id);
     if (!user) {
-      throw new Error('User not found');
+      throw new NotFoundException('User not found');
     }
     return this.repo.remove(user);
   }
